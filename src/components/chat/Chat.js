184e4cb7c202514a0d16aa3671a9d2e0cceba896
Joshua Rodriguez <ewized@gmail.com>
2017-08-11 16:00:58 -0700

    Just stack the headers???

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+
+/** The basic component for displaying the chat dialogs */
 const Dialogs = () => (
   <div>
+    <header>
+      <AppBar title="Chat" iconElementLeft={<IconButton />}/>
+    </header>
     dialogs
     <Link to='/chat/1'>1</Link>
   </div>
 );
 
-const Messages = ({ match: { params: { id } } }) => (
-  <div>messages {id} <Link to='/chat'>Back</Link></div>
-);
+/** The basic component for displaying the messages */
+class Messages extends React.Component {
+
+  render() {
+    let id = this.props.match.params.id;
+    return (
+      <div>
+        <header>
+          <AppBar title="Messages" iconClassNameLeft="mdi mdi-arrow-left" onLeftIconButtonTouchTap={() => {
+            let history = this.props.history;
+            if (history.length > 1) {
+              this.props.history.goBack();
+            } else {
+              this.props.history.go('/chat');
+            }
+          }}/>
+        </header>
+        <pre>
+                  messages {id} {JSON.stringify(this.props, '&nbsp;', 2)}
+        </pre>
+      </div>
+    );
+  }
+}
 
 export default class Chat extends React.Component {
 
