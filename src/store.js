c95dfde52e01f6e190d1426e3b64193fd66e9236
Justin Penka <jpenka@gmail.com>
2017-08-26 03:26:38 -0500

    readded redux-devtools back into project

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import Saga from './saga';
 import reducer from './reducers';
 
@@ -10,17 +10,8 @@ const sagaMiddleware = createSagaMiddleware();
 export default function configStore(history) {
   const middleware = applyMiddleware(routerMiddleware(history), sagaMiddleware);
 
-  // TODO: input reducers dynamically and then combine
-  // create the store
-  //  const store = createStore(
-  //    combineReducers({
-  //      ...reducer,
-  //      router: routerReducer
-  //  }),
-  //  middleware);
-
   // create store
-  const store = createStore(reducer, middleware);
+  const store = createStore(reducer, composeWithDevTools(middleware));
 
   // run the saga middleware
   sagaMiddleware.run(Saga);
