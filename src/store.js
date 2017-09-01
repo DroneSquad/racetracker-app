49d1ab3679b96736c6df2195ea8d7a2b9d435eec
Justin Penka <jpenka@gmail.com>
2017-08-31 21:41:59 -0400

    various setting updated

--- src/store.js
+++ src/store.js
@@ -2,13 +2,14 @@ import { applyMiddleware, createStore } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import Saga from './saga';
 import reducer from './reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default function configStore(history) {
-  const middleware = applyMiddleware(routerMiddleware(history), sagaMiddleware);
+  const middleware = applyMiddleware(routerMiddleware(history), sagaMiddleware, thunk);
 
   // create store
   const store = createStore(reducer, composeWithDevTools(middleware));
