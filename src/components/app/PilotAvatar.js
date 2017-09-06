a6cdca104f48e18c10bccb86d06ddf79ff07fde4
Joshua Rodriguez <ewized@gmail.com>
2017-09-06 10:44:39 -0700

    Smarter image loading

--- src/components/app/PilotAvatar.js
+++ src/components/app/PilotAvatar.js
@@ -6,8 +6,26 @@ import { BLANK_PNG } from '../../utils';
 
 /** Component wrapper for pilot avatars */
 export default class PilotAvatar extends Avatar {
+  state = { complete: false };
+
+  /** This will be called when the image done loading */
+  onLoad = event => {
+    if (event.target.src !== BLANK_PNG) {
+      this.setState({ complete: event.target.complete });
+    }
+  };
+
+  /** Image fails to load replace with blank image */
+  onError = event => {
+    event.target.src = BLANK_PNG;
+  };
+
   render() {
     // todo add lazy loading, and fancy things like image caching
-    return <Avatar {...this.props} className="avatar bar-item" src={BLANK_PNG} style={{...this.props.style, backgroundImage: `url(${this.props.src})`}} />;
+    return (
+      <span className={this.state.complete ? '' : 'loading-bar'} style={{...this.props.style}}>
+        <Avatar onError={this.onError} onLoad={this.onLoad} {...this.props} className="avatar bar-item" src={this.props.src || BLANK_PNG} style={{ objectFit: 'cover' }} />
+      </span>
+    );
   }
 }
