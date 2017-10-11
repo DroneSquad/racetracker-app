34651ea5bad119e29d8bbd0eb214a905a71f1948
Joshua Rodriguez <ewized@gmail.com>
2017-10-11 12:11:27 -0700

    Have the UI properly display the frequencies one to many

--- src/routes/tracker/components/settings/frequencies/Frequencies.js
+++ src/routes/tracker/components/settings/frequencies/Frequencies.js
@@ -17,13 +17,30 @@ export default class Frequencies extends React.Component {
       bands: frequencies.bands,
       profiles: _.map(frequencies.profiles, profile => profile.name),
       amount: 0,
-      channel: 0
+      channel: 0,
+      data: (() => { // todp update with map reduce
+        let data = {};
+        for (let index in frequencies.profiles) {
+          let profile = frequencies.profiles[index];
+          for (let indexBands in profile.frequencies) {
+            let band = profile.frequencies[indexBands];
+            let bands = data[band.bands.length];
+            let i = Number(index);
+            if (bands) {
+              bands.push(i);
+            } else {
+              data[band.bands.length] = [i]
+            }
+          }
+        }
+        return data;
+      })()
     };
   }
 
   /** When the frequency amount changes */
   onFrequencyAmount = (event, value) => {
-    this.setState({ amount: value });
+    this.setState({ amount: value, channel: this.state.channel > this.state.data[value + 1].length - 1 ? 0 : this.state.channel });
   };
 
   /** When the frequency amount changes */
@@ -37,10 +54,8 @@ export default class Frequencies extends React.Component {
   };
 
   render() {
-    let videoProfile = frequencies.profiles[this.state.channel];
-    let selectedAmount =
-      videoProfile.frequencies.length <= this.state.amount ? videoProfile.frequencies.length - 1 : this.state.amount;
-    let videoFrequencies = videoProfile.frequencies[selectedAmount];
+    let videoProfile = frequencies.profiles[this.state.data[this.state.amount + 1][this.state.channel]];
+    let videoFrequencies = _.find(videoProfile.frequencies, freq => freq.bands.length === this.state.amount + 1);
     return (
       <div className="main video-frequencies">
         <header>
@@ -51,12 +66,12 @@ export default class Frequencies extends React.Component {
           />
         </header>
         <main>
-          <DropDownMenu value={selectedAmount} onChange={this.onFrequencyAmount}>
-            {_.range(videoProfile.frequencies.length).map(i => <MenuItem key={i} value={i} primaryText={++i} />)}
+          <DropDownMenu value={this.state.amount} onChange={this.onFrequencyAmount}>
+            {_.map(this.state.data, (array, i) => <MenuItem key={i - 1} value={i - 1} primaryText={i} />)}
           </DropDownMenu>
           <DropDownMenu value={this.state.channel} onChange={this.onFrequencyChannel}>
-            {_.map(this.state.profiles, (profile, index) =>
-              <MenuItem key={index} value={index} primaryText={profile} />
+            {_.map(this.state.data[this.state.amount + 1], (value, index) =>
+              <MenuItem key={index} value={index} primaryText={frequencies.profiles[value].name} />
             )}
           </DropDownMenu>
           <p>
@@ -64,7 +79,7 @@ export default class Frequencies extends React.Component {
             Reduce frequencies to improve timing accuracy.
           </p>
           <List>
-            {videoFrequencies.bands.length > 0 &&
+            {videoFrequencies &&
               _.map(videoFrequencies.bands, (band, index) =>
                 <div key={++index}>
                   <ListItem
@@ -79,8 +94,6 @@ export default class Frequencies extends React.Component {
                   <Divider />
                 </div>
               )}
-            {videoFrequencies.bands.length === 0 &&
-              <ListItem primaryText="There is no profile for this configuration" />}
           </List>
         </main>
         <footer>
