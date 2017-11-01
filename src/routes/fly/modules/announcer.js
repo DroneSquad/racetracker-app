f0f65572468ed14c6574caa990bcde096c677ba5
Justin Penka <jpenka@gmail.com>
2017-11-01 12:59:38 -0400

    changed flyover announce message

--- src/routes/fly/modules/announcer.js
+++ src/routes/fly/modules/announcer.js
@@ -69,7 +69,7 @@ export function announceShotgunStart() {
 
 export function announceFlyoverStart() {
   return dispatch => {
-    dispatch(sendVoice('Flyover start ready'));
+    dispatch(sendVoice('Start when ready'));
   };
 }
 
